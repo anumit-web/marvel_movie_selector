ff7b08ec3c074fa0e6ae027b61cf557d13202565
anumit-web <anumit@gmail.com>
2024-02-13 03:58:14 +0530

    Add files via upload

---
+++ marvel_movie_selector/public/partials/marvel_movie_selector-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://https://github.com/anumit-web
+ * @since      1.0.0
+ *
+ * @package    Marvel_movie_selector
+ * @subpackage Marvel_movie_selector/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
